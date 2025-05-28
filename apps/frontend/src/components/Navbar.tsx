bef0a6c2049ec13ddec1133a30df8c3a34fbded1
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-05-28 01:23:06 +0100

    fix: add render url and port

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
         </Link>
 
         <Link href="/register" className='hidden sm:inline-flex'>
-          <Button className="px-4 py-2 text-sm font-medium text-blue-600 border border-blue-600 rounded-md hover:bg-blue-50">
+          <Button className="px-4 py-2 text-sm font-medium bg-white text-blue-600 border border-blue-600 rounded-md hover:bg-blue-50">
             Create Account
           </Button>
         </Link>
